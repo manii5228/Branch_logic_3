aea2d454a4417d5723633d254e45c2232ad857ac
manii5228 <manimanjunath.v@gmail.com>
2025-08-15 20:54:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -846,6 +846,6 @@ if __name__ == '__main__':
         # Create uploads directory if it doesn't exist
         if not os.path.exists('uploads'):
             os.makedirs('uploads')
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    
+
+    app.run(debug=False, host='0.0.0.0')
